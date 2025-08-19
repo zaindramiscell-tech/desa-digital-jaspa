f2ddef41a6e2438c5b2321eafa46ed9dedb09485
Zaindra Miscell <zaindramiscell@gmail.com>
2025-08-19 14:30:45 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDJWhPQbQ84x5Inp9yoautv5yfwru18

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSy_PLACEHOLDER_FOR_YOUR_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "1:your-sender-id:web:your-app-id",
-  measurementId: "G-XXXXXXXXXX"
+  apiKey: "AIzaSyDJWhPQbQ84x5Inp9yoautv5yfwru18ZZg",
+  authDomain: "desadigital-2a941.firebaseapp.com",
+  projectId: "desadigital-2a941",
+  storageBucket: "desadigital-2a941.appspot.com",
+  messagingSenderId: "8611182099",
+  appId: "1:8611182099:web:0775432f991d7a4eef71e6",
+  measurementId: "G-778EPWCJZY"
 };
 
 // Initialize Firebase
