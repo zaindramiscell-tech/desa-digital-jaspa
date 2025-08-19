717d2cd8cdadcc8ff7c957571632065690616632
Zaindra Miscell <zaindramiscell@gmail.com>
2025-08-19 15:01:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/app/berita/page.tsx
+++ src/app/berita/page.tsx
@@ -40,7 +40,7 @@ export default function BeritaPage() {
     <>
       <section className="relative h-64 w-full flex items-center justify-center text-center text-white">
         <Image
-          src="https://placehold.co/1200x400.png"
+          src="https://plus.unsplash.com/premium_photo-1690527101950-5bc2b81ca773?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Berita Desa"
           fill
           style={{objectFit: 'cover'}}
