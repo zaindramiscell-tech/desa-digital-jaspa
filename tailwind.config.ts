ea2005b736e17f76f9fb4f923505eff6f6005acb
Zaindra Miscell <zaindramiscell@gmail.com>
2025-08-19 14:46:51 +0000

    npm i next-pwa npm i @tanstack/react-query npm i @radix-ui/react-icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Roboto', 'PT Sans', 'sans-serif'],
+        headline: ['Poppins', 'PT Sans', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
